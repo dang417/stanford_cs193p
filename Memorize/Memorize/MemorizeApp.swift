b08f5299c23585e6b7961b9e823e5fc30b4df464
dang417 <dndeo10814@gmail.com>
2023-12-12 16:54:41 +0900

    lecture 2

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 대웅 on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
